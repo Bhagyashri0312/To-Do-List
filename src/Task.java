ed4bfefa1fccbc8a35431dacbda21923b841a194
Bhagyashri0312 <72099148+Bhagyashri0312@users.noreply.github.com>
2020-10-19 16:27:09 +0200

    Update Task.java

--- src/Task.java
+++ src/Task.java
@@ -1,69 +1,70 @@
+/***
+ * Task class has the details of ProjectName, TaskName, TaskDate, Status.
+ */
+
 import java.util.Date;
 
 public class Task {
 
-   private String taskName;
-   private String projectName;
-   private String status;
-   private Date taskDate;
+    private String taskName;
+    private String projectName;
+    private String status;
+    private Date taskDate;
 
-   public Task(String projectName, Date taskDate, String taskName, String status)
-   {
-       this.taskName = taskName;
-       this.projectName= projectName;
-       this.status = status;
-       this.taskDate = taskDate;
-   }
+    public Task(String projectName, Date taskDate, String taskName, String status) {
 
-   public String getTaskName()
-   {
-       return taskName;
-   }
+        this.projectName = projectName;
+        this.taskDate = taskDate;
+        this.taskName = taskName;
+        this.status = status;
 
-   public String getProjectName()
-   {
-       return projectName;
-   }
+    }
 
-   public String getStatus()
-   {
-       return status;
-   }
 
-   public Date getTaskDate()
-   {
-       return taskDate;
-   }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public Date getTaskDate() {
+        return taskDate;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+
 
     /**
      * @set/modify the Status
      */
-    public void setStatus(String setStatus)
-    {
+    public void setStatus(String setStatus) {
         status = setStatus;
     }
 
     /**
      * @set/modify the Task name
      */
-    public void setTaskName(String setTaskName)
-    {
+    public void setTaskName(String setTaskName) {
         taskName = setTaskName;
     }
 
     /**
      * @set/modify the Task Description
      */
-    public void setProjectName(String setProjectName)
-    {
+    public void setProjectName(String setProjectName) {
         projectName = setProjectName;
     }
 
     /**
      * @set/modify the Task Date
      */
-    public void setTaskDate(Date setTaskDate)
-    {
+    public void setTaskDate(Date setTaskDate) {
         taskDate = setTaskDate;
     }
 
