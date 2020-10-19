c3074290aa7cc0e0a9ef62fddf6b82c284a8a492
Bhagyashri0312 <72099148+Bhagyashri0312@users.noreply.github.com>
2020-10-19 16:23:36 +0200

    Update ToDoMain.java

--- src/ToDoMain.java
+++ src/ToDoMain.java
@@ -1,19 +1,11 @@
 import java.io.IOException;
+import java.text.ParseException;
 
+public class ToDoMain {
+    public static void main(String[] args) throws IOException, ParseException {
+ 
 
-public class ToDoMain
-{
-
-    public static void main(String[] args) throws IOException
-    {
-
-
-            }
-
-        }
-
-
-
-
-
-
+        ToDoList doOption = new ToDoList();
+        doOption.displayOption();
+    }
+}
